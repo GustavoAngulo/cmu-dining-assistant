f534aab9aeba9d5cb04b28225429867f33c55e65
GustavoAngulo <gangulo@andrew.cmu.edu>
2016-12-24 11:36:21 -0500

    Added Webhook framework, changes to README

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # CMU Dining Assistant
 
 ## About
-CMU Dining Assistant is a Google Assistant Plug in that gives users information on CMU dining facilities
+CMU Dining Assistant is a Google Assistant Plug-in that gives users information on CMU dining facilities
 
 ## Technologies
 API.AI
+
 Heroku
+
 [CMU ScottyLabs Dining API](https://scottylabs.org/dining-api/)
 
--- app.py
+++ app.py
@@ -1,10 +1,68 @@
 import requests
-from unidecode import unidecode
-import ast
 import json
+import flask
+# from flask import Flask
+# from flask import request
+# from flask import make_response
+from unidecode import unidecode
+
+# Help taken from https://github.com/svet4/shipping-costs-sample/blob/master/app.py
 
 locations = requests.get('http://apis.scottylabs.org/dining/v1/locations')
 locations = locations.json()
+CMU_Dining_Dict = {}
 for i in range(len(locations["locations"])):
-    print(unidecode(locations["locations"][i]["name"]))
-    print(unidecode(locations["locations"][i]["location"]))
+    CMU_Dining_Dict[unidecode(locations["locations"][i]["name"])] = locations["locations"][i]
+
+# for i in range(len(locations["locations"])):
+#     print(unidecode(locations["locations"][i]["name"]))
+#     print(unidecode(locations["locations"][i]["location"]))
+
+
+
+
+app = flask.Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+
+def webhook():
+    req = flask.request.get_json(silent=True, force=True)
+
+    print("Request:")
+    print(json.dumps(req, indent=4))
+
+    res = makeWebhookResult(req)
+
+    res = json.dumps(res, indent=4)
+    print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+
+def makeWebhookResult(req):
+    if req.get("result").get("action") == "get_time":
+        speech = get_time_response(req)
+    else:
+        speech = {}
+
+    print("Response:")
+    print(speech)
+
+    return {
+        "speech": speech,
+        "displayText": speech,
+        #"data": {},
+        # "contextOut": [],
+        "source": "apiai-onlinestore-shipping"
+    }
+
+def get_time_response(req):
+    result = req.get("result")
+    parameters = result.get("parameters")
+    location = parameters.get("location")
+    location_status = parameters.get("location-status")
+    if location_status == "open":
+        time_hour = CMU_Dining_Dict[location]["times"][0]
+
+
