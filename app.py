02af3cbd6e66e8830ee54ed0f1be97740fee8cc7
GustavoAngulo <gangulo@andrew.cmu.edu>
2016-12-25 21:32:08 -0500

    Bug with flask

--- app.py
+++ app.py
@@ -6,14 +6,6 @@ from response_functions.get_time_response import get_time_response
 
 # Help taken from https://github.com/svet4/shipping-costs-sample/blob/master/app.py
 
-locations = requests.get('http://apis.scottylabs.org/dining/v1/locations')
-locations = locations.json()
-CMU_Dining_Dict = {}
-for i in range(len(locations["locations"])):
-    CMU_Dining_Dict[unidecode(locations["locations"][i]["name"])] = locations["locations"][i]
-
-
-
 
 
 app = flask.Flask(__name__)
@@ -21,6 +13,14 @@ app = flask.Flask(__name__)
 @app.route('/webhook', methods=['POST'])
 
 def webhook():
+    
+    locations = requests.get('http://apis.scottylabs.org/dining/v1/locations')
+    locations = locations.json()
+    CMU_Dining_Dict = {}
+    for i in range(len(locations["locations"])):
+        CMU_Dining_Dict[unidecode(locations["locations"][i]["name"])] = locations["locations"][i]
+
+
     req = flask.request.get_json(silent=True, force=True)
 
     print("Request:")
@@ -30,7 +30,7 @@ def webhook():
 
     res = json.dumps(res, indent=4)
     print(res)
-    r = make_response(res)
+    r = flask.make_response(res)
     r.headers['Content-Type'] = 'application/json'
     return r
 
