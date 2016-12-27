96966145383d03114818571e44d5f170375c2ec6
GustavoAngulo <gangulo@andrew.cmu.edu>
2016-12-27 15:54:06 -0500

    Added returning both open/close times

--- response_functions/get_time_response.py
+++ response_functions/get_time_response.py
@@ -16,6 +16,7 @@ def get_time_response(req, CMU_dining_dict):
                       "on Friday.", 
                       "on Saturday."]
 
+    # reqDate: 0 is Sunday, 1 Monday, ..., 6 Saturday             
     if reqDate == "":
         reqDate = (date.today().weekday() + 1) % 7
         speech_date = "today."
@@ -26,49 +27,33 @@ def get_time_response(req, CMU_dining_dict):
                         int(reqDate[2])).weekday() + 1) % 7
         speech_date = date_responses[reqDate]
 
-    # reqDate: 0 is Sunday, 1 Monday, ..., 6 Saturday
 
 
     if location_status == "open":
-        location_status = "start"
-    else:  
-        location_status = "end"
-     
-    
-    hour, minute = get_time(location, location_status, reqDate, CMU_dining_dict)
+        time = modify_time(get_time(location, "start", reqDate, CMU_dining_dict))
 
-    if hour == None and minute == None:
+    elif location_status == "close": 
+        time = modify_time(get_time(location, "end", reqDate, CMU_dining_dict))
+    else:
+        time_start = modify_time(get_time(location, "start", reqDate, CMU_dining_dict))
+        time_end = modify_time(get_time(location, "end", reqDate, CMU_dining_dict))
+
+    if time == None or time_start == None or time_end == None:
         return (location + " is not open " + speech_date)
     else:
-        # changing military time to standard time
-        if hour == 0:
-            (hour, time_suffix) = (12, "am")
-        elif hour < 12:
-            (hour, time_suffix) = (hour, "am")
-        elif hour == 12:
-            (hour, time_suffix) = (hour, "pm")
+        
+        if location_status == "open":
+            return (location + " opens at " + time + " " + speech_date)
+        
+        elif location_status == "close"
+            return (location + " closes at " + time + " " + speech_date)
+
+        elif location_status == "":
+            return (location + " opens at " + time_start + 
+                    " and closes at " + time_end + " " + speech_date)
+            
         else:
-            (hour, time_suffix) = (hour - 12, "pm")
-
-        # prevents 8:5 instead of 8:05
-        if minute < 10:
-            minute = "0" + str(minute)
-        else:
-            minute = str(minute)
-
-        if location_status == "start":
-            return (location + (" opens at %d:" % (hour)) 
-                        + minute 
-                        + " " + time_suffix 
-                        + " " + speech_date)
-        else:
-            return (location + (" closes at %d:" % (hour)) 
-                        + minute 
-                        + " " + time_suffix 
-                        + " " + speech_date)
-
-
-
+            return ("There was an error in retrieving the times for " + location)
 
 
 def get_time(location, status, date, CMU_dining_dict):
@@ -93,4 +78,32 @@ def get_time(location, status, date, CMU_dining_dict):
         return (None,None)
 
 
+def modify_time(hour, minute)
+    # hour
+    #   type: int
+    #   content: hour time 0-23
+    #
+    # minute
+    #   type: int
+    #   content: minute time 0-59
 
+    if hour == None and minute == None:
+        return None
+
+    # changing military time to standard time and returning it as a string
+    if hour == 0:
+        (hour, time_suffix) = ("12", "am")
+    elif hour < 12:
+        (hour, time_suffix) = (str(hour), "am")
+    elif hour == 12:
+        (hour, time_suffix) = (str(hour), "pm")
+    else:
+        (hour, time_suffix) = (str(hour - 12), "pm")
+
+    # prevents 8:5 instead of 8:05
+    if minute < 10:
+        minute = "0" + str(minute)
+    else:
+        minute = str(minute)
+
+    return (hour + ":" + minute + " " + time_suffix)
